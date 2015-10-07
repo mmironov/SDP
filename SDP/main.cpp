544588feb01ea688b8ca23c83a88aa37f8c9ff17
Miroslav <miroslavmironov@gmail.com>
2015-10-07 12:18:25 +0300

    Initial Commit

---
+++ SDP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDP
+//
+//  Created by Miroslav Mironov on 10/7/15.
+//  Copyright © 2015 Miroslav Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
